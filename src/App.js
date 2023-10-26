216485dec428ef681ca0b9359e18a35719691d82
Sara <saraxurax732@gmail.com>
2023-10-26 10:50:56 +0100

    admin

--- src/App.js
+++ src/App.js
@@ -10,17 +10,19 @@ import Map from "./home-components/map";
 import RioFooter from "./home-components/footer";
 import Whatsapp from "./components/whatsapp";
 import ScrollToTop from "./components/scrollToTop";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Contatto from "./home-components/contact-form";
-import {useState, useEffect} from "react";
-import Loader from "./components/loader"
-export default function App() {
+import SignIn from "./admin/sign-in";
+import { useState, useEffect } from "react";
+import Loader from "./components/loader";
+import { Container } from "react-bootstrap";
 
+export default function App() {
   const [loaded, setLoaded] = useState(false);
 
-//   the useEffect will run on the first rendering of the App component
-//   after two seconds (about how long it takes for the data to load)
-//   the loaded state will become true
+  //   the useEffect will run on the first rendering of the App component
+  //   after two seconds (about how long it takes for the data to load)
+  //   the loaded state will become true
   useEffect(() => {
     let timer = setTimeout(() => setLoaded(true), 2000);
     return () => {
@@ -29,38 +31,49 @@ export default function App() {
   }, []);
 
   return (
-    <>{!loaded? <Loader/>: <>
-      <Navigation />
-       <CarouselUno />
-       <div className="spacer"></div>
-       <Routes>
-         <Route
-           path="/"
-           element={
-             <>
-               <About id="about" />
-               <div className="spacer"></div>
-               <Counter />
-               <BookingsForm id="prenota" />
-               <div className="spacer"></div>
-               <Gallery id="galleria" />
-               <div className="spacer"></div>
-               <Testimonials />
-               <div className="spacer"></div>
-               <Rooms id="camere" />
-               <div className="spacer"></div>
-               <Contatto id="contatto" />
-               <div className="spacer"></div>
-               <Map /> 
-             </>
-          }
-        />
-      </Routes>
-      <RioFooter />
-      <Whatsapp />
-      <ScrollToTop />
-    </>}
-     
+    <>
+      {!loaded ? (
+        <Loader />
+      ) : (
+        <>
+          <Navigation />
+          <CarouselUno />
+          <div className="spacer"></div>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <About id="about" />
+                  <div className="spacer"></div>
+                  <Counter />
+                  <BookingsForm id="prenota" />
+                  <div className="spacer"></div>
+                  <Gallery id="galleria" />
+                  <div className="spacer"></div>
+                  <Testimonials />
+                  <div className="spacer"></div>
+                  <Rooms id="camere" />
+                  <div className="spacer"></div>
+                  <Contatto id="contatto" />
+                  <div className="spacer"></div>
+                  <Map />
+                </>
+              }
+            />
+            <Route />
+            <Route path="/login" element={<SignIn />} />
+          </Routes>
+          <RioFooter />
+          <Whatsapp />
+          <ScrollToTop />
+          <Container className="admin-container" fluid>
+            <Link className="admin-link" to="/login">
+              Admin
+            </Link>
+          </Container>
+        </>
+      )}
     </>
   );
 }
